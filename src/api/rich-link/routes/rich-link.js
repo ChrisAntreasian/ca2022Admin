39810dca8dacaa57216e52cc622ccd4986eee07c
Chris Antreasian <chrisantreasian@gmail.com>
2022-07-31 21:00:54 -0400

    model updates ignore package

---
+++ src/api/rich-link/routes/rich-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rich-link router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::rich-link.rich-link');
---
+++ src/api/rich-link/services/rich-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rich-link service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::rich-link.rich-link');
