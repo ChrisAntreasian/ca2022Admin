4599b29a19baa637528dbb90d964392816b656f0
Chris Antreasian <chrisantreasian@gmail.com>
2022-09-07 09:20:02 -0400

    update client config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,16 +3,13 @@ const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, DATABASE_USER, DATABASE_PAS
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'mysql',
     connection: {
       host: DATABASE_HOST,
       port: DATABASE_PORT,
       database: DATABASE_NAME,
       user: DATABASE_USER,
-      password: DATABASE_PASSWORD,
-      ssl: {
-        rejectUnauthorized: false
-      },
+      password: DATABASE_PASSWORD
     },
     debug: false,
   },
