3f7f5751bcebd44621844466adf78fe49d7336a8
Chris Antreasian <chrisantreasian@gmail.com>
2022-09-09 10:12:50 -0400

    add url remove host

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,7 +2,7 @@
 const { PORT, HOST, APP_KEYS } = process.env;
 
 module.exports = ({ env }) => ({
-  host: env("HOST", HOST),
+  url: env('http://caAdmin.cleaver.rocks'),
   port: env("PORT", PORT),
   app: {
     keys: env.array('APP_KEYS', APP_KEYS),
