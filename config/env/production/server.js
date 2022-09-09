6863cd166be7824707441767a502c2b67979ea5f
Chris Antreasian <chrisantreasian@gmail.com>
2022-09-09 13:29:16 -0400

    update server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,9 @@
 
-const { PORT, HOST, APP_KEYS } = process.env;
+const { PORT, HOST, APP_KEYS, URL } = process.env;
 
 module.exports = ({ env }) => ({
-  url: env('http://caAdmin.cleaver.rocks'),
+  host: env("HOST", HOST),
+  url: env(URL),
   port: env("PORT", PORT),
   app: {
     keys: env.array('APP_KEYS', APP_KEYS),
