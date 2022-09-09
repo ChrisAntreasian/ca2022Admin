f2052c9e518faa983af1151538d3bf24abd4f0aa
Chris Antreasian <chrisantreasian@gmail.com>
2022-09-08 22:18:44 -0400

    back to postgress

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@ const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, DATABASE_USER, DATABASE_PAS
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
       host: DATABASE_HOST,
       port: DATABASE_PORT,
