ffc4ce7443e5a052e029b6080af2a3a146d4e366
Chris Antreasian <chrisantreasian@gmail.com>
2022-09-09 01:37:02 -0400

    add configs

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,10 @@
 
-const { PORT } = process.env;
+const { PORT, HOST, APP_KEYS } = process.env;
 
 module.exports = ({ env }) => ({
-  port: PORT
+  host: env("HOST", HOST),
+  port: env("PORT", PORT),
+  app: {
+    keys: env.array('APP_KEYS', APP_KEYS),
+  },
 });
