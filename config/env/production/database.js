4742b14336d31f6fd1c6fc956e1526815e643283
Chris Antreasian <chrisantreasian@gmail.com>
2022-10-30 21:59:25 -0400

    use configs from the docs

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,13 +4,21 @@ const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, DATABASE_USER, DATABASE_PAS
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
+    // connection: {
+    //   host: DATABASE_HOST,
+    //   port: DATABASE_PORT,
+    //   database: DATABASE_NAME,
+    //   user: DATABASE_USER,
+    //   password: DATABASE_PASSWORD
+    // },
     connection: {
-      host: DATABASE_HOST,
-      port: DATABASE_PORT,
-      database: DATABASE_NAME,
-      user: DATABASE_USER,
-      password: DATABASE_PASSWORD
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
+    useNullAsDefault: true,
     debug: false,
   },
 });
